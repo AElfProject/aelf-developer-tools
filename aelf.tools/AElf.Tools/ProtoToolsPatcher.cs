973f0be5017275977115749d8cacb1afb5f9e3ee
yimeng.lu <yimeng.lu@aelf.io>
2023-04-24 18:37:20 +0800

    add patcher

--- aelf.tools/AElf.Tools/ProtoToolsPatcher.cs
+++ aelf.tools/AElf.Tools/ProtoToolsPatcher.cs
@@ -1,34 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading;
-using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
-
-namespace AElf.Tools;
-
-public class ProtoToolsPatcher : Task
-{
-
-    public override bool Execute()
-    {
-        // 加载程序集
-        Assembly assembly = Assembly.Load(new AssemblyName("AElf.ContractDeployer.dll"));
-        // 获取 Program 类型
-        Type programType = assembly.GetType("AElf.ContractDeployer.Program");
-        // 获取 Main 方法
-        MethodInfo mainMethod = programType.GetMethod("Main", BindingFlags.Static | BindingFlags.NonPublic);
-        // 构建option参数
-        var optionsType = assembly.GetType("AElf.ContractDeployer.Program+Options");
-        var optionsInstance = Activator.CreateInstance(optionsType);
-        optionsType.GetProperty("ContractDllPath").SetValue(optionsInstance, "*.dll");
-        optionsType.GetProperty("IsSystemContract").SetValue(optionsInstance, true);
-        var args = new[] { optionsInstance };
-        // 调用 Main 方法
-        if (mainMethod != null) mainMethod.Invoke(null, new object[] {args});
-        return true;
-    }
-
-}
+// using System;
+// using System.Collections.Generic;
+// using System.IO;
+// using System.Linq;
+// using System.Reflection;
+// using System.Threading;
+// using Microsoft.Build.Framework;
+// using Microsoft.Build.Utilities;
+//
+// namespace AElf.Tools;
+//
+// public class ProtoToolsPatcher : Task
+// {
+//
+//     public override bool Execute()
+//     {
+//         Assembly assembly = Assembly.Load(new AssemblyName("AElf.ContractDeployer.dll"));
+//         Type programType = assembly.GetType("AElf.ContractDeployer.Program");
+//         MethodInfo mainMethod = programType.GetMethod("Main", BindingFlags.Static | BindingFlags.NonPublic);
+//         var optionsType = assembly.GetType("AElf.ContractDeployer.Program+Options");
+//         var optionsInstance = Activator.CreateInstance(optionsType);
+//         optionsType.GetProperty("ContractDllPath").SetValue(optionsInstance, "*.dll");
+//         optionsType.GetProperty("IsSystemContract").SetValue(optionsInstance, true);
+//         var args = new[] { optionsInstance };
+//         if (mainMethod != null) mainMethod.Invoke(null, new object[] {args});
+//         return true;
+//     }
+//
+// }
