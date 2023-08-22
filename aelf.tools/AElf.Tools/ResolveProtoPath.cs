7add9ff1b6b4da4d589c5049dce42ddf0d219da3
aelf-lu <yimeng.lu@aelf.io>
2023-08-22 10:25:59 +0800

    fix: fix reference path issue

--- aelf.tools/AElf.Tools/ResolveProtoPath.cs
+++ aelf.tools/AElf.Tools/ResolveProtoPath.cs
@@ -16,14 +16,21 @@ namespace AElf.Tools
         [Output]
         public string[] TargetBaseProto { get; set; }
         [Output]
+        public string[] TargetBasePath { get; set; }
+        [Output]
         public string[] TargetRefProto { get; set; }
+        [Output]
+        public string[] TargetRefPath { get; set; }
         
         public override bool Execute()
         {
             List<string> baseList = new List<string>();
             List<string> refList = new List<string>();
+            List<string> basePathList = new List<string>();
+            List<string> refPathList = new List<string>();
             foreach (var packageReference in ProtoAllRef)
             {
+                // todo TargetDir name
                 var targetDir = string.IsNullOrEmpty(packageReference.GetMetadata("TargetDir")) ? "base" : packageReference.GetMetadata("TargetDir");
                 var identity = packageReference.GetMetadata("Identity");
                 var version = packageReference.GetMetadata("Version");
@@ -31,21 +38,27 @@ namespace AElf.Tools
                 var name = identity.Split('.')[1].ToLower();
 
                 // var path = identity + "/" + version + "/" + targetDir + "/" + "Protobuf/";
-                var path = NugetRoot + identity.ToLower() + "/" + version + "/" + "Protobuf/" + name + ".proto";
-                Console.WriteLine($"path: {path}");
-
+                var proto = NugetRoot + identity.ToLower() + "/" + version + "/" + "Protobuf/" + name + ".proto";
+                Console.WriteLine($"path: {proto}");
+                var path = NugetRoot + identity.ToLower() + "/" + version + "/" + "Protobuf";
+                
+                // todo 
                 if (targetDir.Equals("base"))
                 {
-                    baseList.Add(path);
+                    baseList.Add(proto);
+                    basePathList.Add(path);
                 }
                 else if (targetDir.Equals("reference"))
                 {
-                    refList.Add(path);
+                    refList.Add(proto);
+                    refPathList.Add(path);
                 }
             }
 
             TargetBaseProto = baseList.ToArray();
+            TargetBasePath = basePathList.ToArray();
             TargetRefProto = refList.ToArray();
+            TargetRefPath = refPathList.ToArray();
             
             foreach (var p in TargetBaseProto)
             {
